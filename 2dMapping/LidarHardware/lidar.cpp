075948672d39539ccd50d2a55a62299bfa93d8d3
Justin Finn <jfinn5@ut.utm.edu>
2025-09-09 18:45:04 -0500

    Changed the structure of .cpp, added a vector to hold the cartesian xyz points, made a function to save them to a file.. I have also added some minor filtering to make sure the points dont exceed any bounds we would want in the future

--- 2dMapping/LidarHardware/lidar.cpp
+++ 2dMapping/LidarHardware/lidar.cpp
@@ -2,18 +2,23 @@
 #include <string>
 #include "sl_lidar.h"
 #include "sl_lidar_driver.h"
-
+#include <vector>
+#include <fstream>
 using namespace sl;
 
 /* GOALS:
 1.) We need to put together our lidar camera to try to handle 3d computing
 2.) We need to start implementing the coordinates to send to the csv file
 3.) We need to get the iterative closest point algorithm working*/
+struct cartesian{
+    float x_coordinate, y_coordinate, z_coordinate;
+};
 
 int findX(const float &angle, const float &distance);
 int findY(const float &angle, const float &distance);
 int findZ(const float &angle, const float &distance);
-void filterData();
+void saveToFile(std::vector<cartesian> points);
+
 int main() {
     std::cout << "Starting LIDAR test..." << std::endl;
     std::string serial_port;
@@ -57,8 +62,16 @@ int main() {
         return -1;
     }
 
+
+
     sl_lidar_response_measurement_node_hq_t nodes[8192];
     size_t   count = sizeof(nodes) / sizeof(nodes[0]);
+    const float min_distance = 0.0f;
+    const float max_distance = 250.0f;
+    const float min_angle = 0.0f;
+    const float max_angle = 2500.0f;
+    
+    std::vector<cartesian> finished_points;
 
     if (SL_IS_OK(drv->grabScanDataHq(nodes, count))) {
         drv->ascendScanData(nodes, count);
@@ -66,12 +79,23 @@ int main() {
         for (size_t i = 0; i < count; ++i) {
             float angle = (nodes[i].angle_z_q14 * 90.f) / 16384.f;
             float dist  = nodes[i].dist_mm_q2 / 4.0f;
-            std::cout << angle << "," << dist << std::endl;
+
+            if(dist >= min_distance || dist <= max_distance) continue;
+            if(angle >= min_angle || angle <= max_angle) continue;
+
+            cartesian coordinate;
+
+            coordinate.x_coordinate = findX(angle, dist);
+            coordinate.y_coordinate = findY(angle, dist);
+            coordinate.z_coordinate = findZ(angle, dist);
+
+            finished_points.push_back(coordinate);
         }
     } else {
         std::cerr << "Failed to grab scan data." << std::endl;
     }
 
+    saveToFile(finished_points);
     drv->stop();
     drv->setMotorSpeed(0);
 
@@ -99,6 +123,16 @@ int findZ(const float &angle, const float &distance){
     return x; 
 }
 
-void FilterData(){
+void saveToFile(std::vector<cartesian> points){
+    std::string file_name = "sorted_xyz.csv";
+    std::ofstream file(file_name);
 
+    if(!file.is_open()){
+        std::cerr << "The file has failed to open; possibly failed" << std::endl;
+        std::cout << "Filename tried: " << file_name << std::endl;
+    }
+
+    for(const auto& p: points){
+        file << p.x_coordinate << p.y_coordinate << p.z_coordinate << std::endl; 
+    }
 }
