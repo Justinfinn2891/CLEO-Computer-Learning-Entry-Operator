cb7b1804db7bbe757849923493c3453c64cec2e9
Justin <jfinn5@ut.utm.edu>
2025-09-16 20:18:36 -0500

    changes that will hopefully fix the motor class

--- 2dMapping/LidarHardware/include/motor.h
+++ 2dMapping/LidarHardware/include/motor.h
@@ -3,25 +3,34 @@
 
 
 // Libraries used for the step motor 
-#include <wiringPi.h> // USING FOR PINMODE() and digitalWrite()
+#include "wiringPi.h" // USING FOR PINMODE() and digitalWrite()
 #include <unistd.h> // Need for sleep functions 
 #include <iostream>
 
-const int NUM_X = 8
-const int NUM_Y = 4
+const int NUM_X = 8;
+const int NUM_Y = 4;
 class Motor{
 
     private:
-        int steps[NUM_X][NUM_Y];
-        int Pin1, Pin2, Pin3, Pin4;
+        int steps[NUM_X][NUM_Y] = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 1},
+        {1, 0, 0, 1}
+    };
+        
 
     public:
   
         Motor();
 
-
+        int Pin1, Pin2, Pin3, Pin4;
         // FUNCTIONS
-        void stepMotor(const float &step);
+        void stepMotor(const int &step);
         void forward(const int &stepCount, const int &delayMs = 2);
         void backward(const int &stepCount, const int &delayMs = 2);
 };
